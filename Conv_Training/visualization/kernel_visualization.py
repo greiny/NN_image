21b231f94c8f360979cd289cc8eafa2f8404da62
greiny <greiny@naver.com>
2018-07-13 11:20:32 +0900

    visualization update

--- Conv_Training/visualization/kernel_visualization.py
+++ Conv_Training/visualization/kernel_visualization.py
@@ -4,40 +4,41 @@ import csv
 import os.path
 
 padding = 1
-for num in range(10):
-    filename = 'kernel_%d/ch_0.csv' % num
+for n in range(10):
+    for num in range(10):
+        filename = 'layer_%d/kernel_%d/ch_0.csv' % (n,num)
 
-    if os.path.isfile(filename) :
-        figname = 'kernel_%d' % (num)
-        data = np.genfromtxt(filename, delimiter=",")
+        if os.path.isfile(filename) :
+            figname = 'layer_%d/kernel_%d' % (n,num)
+            data = np.genfromtxt(filename, delimiter=",")
 
         # Assume he filters are squarnume
-        filter_size = data.shape[1]
+            filter_size = data.shape[1]
         # Size of the result image including padding
-        result_size = (filter_size + padding) - padding -1
+            result_size = (filter_size + padding) - padding -1
         # Initialize result image to all zeros
-        result = np.zeros((result_size, result_size))
+            result = np.zeros((result_size, result_size))
 
         # Tile the filters into the result image
-        filter_x = 0
-        filter_y = 0
+            filter_x = 0
+            filter_y = 0
 
-        for i in range(filter_size-1):
-            for j in range(filter_size-1):
-                result[filter_y*(filter_size + padding) + i, filter_x*(filter_size + padding) + j] = data[i, j]
-        filter_x += 1
+            for i in range(filter_size-1):
+                for j in range(filter_size-1):
+                    result[filter_y*(filter_size + padding) + i, filter_x*(filter_size + padding) + j] = data[i, j]
+            filter_x += 1
 
         # Normalize image to 0-1
-        min = result.min()
-        max = result.max()
-        result = (result - min) / (max - min)
+            min = result.min()
+            max = result.max()
+            result = (result - min) / (max - min)
 
         # Plot figure
-        plt.figure(figsize=(7,7))
-        plt.axis('off')
-        plt.imshow(result, cmap='gray', interpolation='nearest')
+            plt.figure(figsize=(7,7))
+            plt.axis('off')
+            plt.imshow(result, cmap='gray', interpolation='nearest')
 
         # Save plot if filename is set
-        if figname != '':
-            plt.savefig(figname, bbox_inches='tight', pad_inches=0)
+            if figname != '':
+                plt.savefig(figname, bbox_inches='tight', pad_inches=0)
 
